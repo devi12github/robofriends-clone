7a1a6c0a8398bbaee2de72ef85ad371f549e56fd
Rahul <rahulscodingworld@gmail.com>
2022-01-13 16:13:58 -0500

    fixed scrolling method

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div style={{overflowY: 'scroll', height:'900px'}}>
+        <div style={{overflowY: 'scroll', height:'500px'}}>
             {props.children}
         </div>
     )
